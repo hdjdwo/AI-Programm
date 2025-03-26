6c72b7b24db2fad1cf165bd6255a294ad987f90b
Vlad Volohovich <vlad.1488skaskevich@gmail.com>
2025-03-27 00:06:15 +0300

    add TS

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let message = "Hello, TypeScript!";
+console.log(message);
+// Пример 2: Типизация функции
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 3));
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+let message: string = "Hello, TypeScript!";
+console.log(message);
+
+// Пример 2: Типизация функции
+function sum(a: number, b: number): number {
+  return a + b;
+}
+console.log(sum(5, 3));
