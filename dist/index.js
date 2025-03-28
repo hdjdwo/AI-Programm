6acd9825ece3c1cad9ccf577cb055d85b2478301
Vlad Volohovich <vlad.1488skaskevich@gmail.com>
2025-03-28 18:32:48 +0300

    create new folder

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let message = "Hello, TypeScript!";
-console.log(message);
-// Пример 2: Типизация функции
-function sum(a, b) {
-    return a + b;
-}
-console.log(sum(5, 3));
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-let message: string = "Hello, TypeScript!";
-console.log(message);
-
-// Пример 2: Типизация функции
-function sum(a: number, b: number): number {
-  return a + b;
-}
-console.log(sum(5, 3));
